2cb5e675bc7fbdd8c93ac6498f053accf6b2597d
jose385 <33008494+jose385@users.noreply.github.com>
2025-07-05 20:22:09 +0000

    changes

--- loader/load_parquet_into_pg.py
+++ loader/load_parquet_into_pg.py
@@ -20,7 +20,7 @@ for fp in glob.glob("stage/*.parquet"):
 
     cols = ','.join(df.columns)
 
-    your_file = "stage/statcast_pitchlog.parquet"  
+    your_file = "stage/*.parquet"  
 
     with open(your_file, "rb") as f, conn.cursoe() as cur:
 
